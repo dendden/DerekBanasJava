6d8e22747278bbd6d6cf94baa992d8665b3b0341
Denys Triasunov <dendden@users.noreply.github.com>
2016-01-19 22:56:09 +0200

    Added string var to print

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -1,5 +1,7 @@
 public class HelloWorld1 {
+  static String randomString = "String to print";
+
   public static void main (String[] args) {
-    System.out.println("Hello World");
+    System.out.println(randomString);
   }
 }
