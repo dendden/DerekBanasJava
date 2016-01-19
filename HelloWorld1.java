907118dc51d6f628071c4997245c0151c2d199fb
Denys Triasunov <dendden@users.noreply.github.com>
2016-01-19 23:14:01 +0200

    Added static final double PINUM

--- HelloWorld1.java
+++ HelloWorld1.java
@@ -1,7 +1,8 @@
 public class HelloWorld1 {
   static String randomString = "String to print";
+  static final double PINUM = 3.141529;
 
   public static void main (String[] args) {
-    System.out.println(randomString);
+    System.out.println(PINUM);
   }
 }
