b216c1045e5e8f4146a9afde8a2cd898926ec6e1
Denys Triasunov <dendden@users.noreply.github.com>
2016-01-27 13:21:58 +0200

    main() func is now handling exceptions thrown by getFile() function

--- Lesson6/src/Lesson6.java
+++ Lesson6/src/Lesson6.java
@@ -3,25 +3,16 @@ import java.io.*;
 public class Lesson6 {
 	
 	public static void main(String[] args) {
-		getFile("./some_shit.txt");
+		try {
+			getFile("./some_shit.txt");
+		}
+		catch (IOException e) {
+			System.out.println("An IO error occurred");
+		}
 	}
 	
-	public static void getFile(String fileName) {
-		try {
-			FileInputStream file = new FileInputStream(fileName);
-		}
-		catch (FileNotFoundException e) {
-			System.out.println("Sorry, can't find this file");
-		}
-//		catch (IOException e) {
-//			System.out.println("Unknown IO error...");
-//		}
-//		catch (ClassNotFoundException e) {} // <= ignoring exception
-		catch (Exception e) {
-			System.out.println("Some weird error occurred...");
-		}
-		finally {
-			System.out.println("");
-		}
+	public static void getFile(String fileName)  throws FileNotFoundException,
+	IOException {
+		FileInputStream file = new FileInputStream(fileName);
 	}
 }
